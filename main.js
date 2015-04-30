142507282492dae777c79076b52951b4f9ce4785
Dustin Bachrach <ahdustin@gmail.com>
2015-04-30 09:39:47 -0700

    fmedia learned the --meet command Takes two movies and shows related movies

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ version.fetch(function(error, v) {
     program
       .version(v)
       .description('Search for movies and where to watch them')
+      .option('-m, --meets <query>', 'Find media that relates to this\'n\'that')
       .arguments('<query...>')
       .action(function(queryParams) {
         var mediaType = 'movie';
         var query = queryParams.join(' ');
 
         controller.configure().then(function() {
-          controller.query(query, mediaType);
+          if (program.meets) {
+            controller.thisMeetsThat(query, program.meets, mediaType);
+          }
+          else {
+            controller.query(query, mediaType);
+          }
         });
       })
       .parse(process.argv);
